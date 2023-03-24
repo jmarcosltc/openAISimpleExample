6f32a1be56a27390d4f19aeb27041f28144f1da6
unknown <joao163marcos@gmail.com>
2023-03-23 22:13:00 -0300

    ajustes e melhoras

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ const fs = require('fs')
 import axios from 'axios'
 require('dotenv').config({ path: '../.env'})
 
-console.log(process.env.APIKEY)
-
 const conf = new Configuration({
-    apiKey: process.env.APIKEY,
+    apiKey: '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',
 })
 
 const genImg = async (pedido: string) => {
@@ -19,15 +17,26 @@ const genImg = async (pedido: string) => {
     })
     let image_url = res.data.data[0].url
     
+    console.log(image_url)
     const imagem = await download_image(image_url)
     const buffer = Buffer.from(imagem, "base64");
     fs.writeFileSync(`./storage/${pedido}.jpg`, buffer)
 }
 
 const download_image = (url: any) => {
+
+    fs.mkdir('./storage', function(e: any){
+        if(!e || (e && e.code === 'EEXIST')){
+            //do something with contents
+        } else {
+            //debug
+            console.log(e);
+        }
+    });
+
     return axios.get(url, {
         responseType: 'arraybuffer'
     }).then(response => Buffer.from(response.data, 'binary').toString('base64'))
 }
 
-genImg('manuel games')
+genImg('t-rex driving a school bus')
